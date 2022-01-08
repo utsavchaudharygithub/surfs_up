f9a69d71fb7d7d5414aa7e670497565d0449dc8b
SHERPA 944allview CHAUDHARY <944allview@SHERPAs-iMac.local>
2022-01-08 11:00:38 -0800

    adding py and ipynb

--- app.py
+++ app.py
@@ -76,3 +76,6 @@ def stats(start=None, end=None):
          filter(Measurement.date <= end).all()
     temps = list(np.ravel(results))
     return jsonify(temps=temps)
+
+if __name__ == "__main__":
+    app.run(debug = True)
